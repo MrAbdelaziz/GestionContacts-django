922536a08177ab360ea02d2e50f7e7c8a4a7df08
MrAbdelaziz <elouahab.abdelaziz@gmail.com>
2020-05-20 12:17:47 +0000

    admin.py : list_display list_filter search_fields ordering

--- contact/admin.py
+++ contact/admin.py
@@ -1,11 +1,15 @@
+from re import search
+
 from django.contrib import admin
 from contact.models import *
 
 class contactModel(admin.ModelAdmin):
     list_display = ('nom','adresse','tel','email')
     list_filter = ('nom','tel')
+    search_fields = ('nom',)
+    ordering = ('tel',)
 
 admin.site.register(User)
-admin.site.register(Contact)
+admin.site.register(Contact,contactModel)
 admin.site.register(Categorie)
 
